868c0024e70c9c7fa4b8292d58cd710552796bda
juntae <rlawnsxo131@gmail.com>
2022-12-06 23:47:49 +0900

    comment: add comment

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 /**
- * 이 경우는 berry 에서 권장하는 방법과는 거리가 먼듯하다.
- * 예제용으로 작성한 프로젝트이므로 예시용도로 남겨둔다(dependency 포함)
+ * 이 경우는 berry 에서 권장하는 방법은 아니란 생각이 들기도 한다.
+ * 다만, root 만 jest 를 갖고, yarn workspace 로 test 를 실행할순 있는데...
+ * 일단 이건 나중에 조금더 고민해 보기로 하자.
+ * 예제용으로 작성한 프로젝트이므로 예시용도로 남겨둔다(dependency 포함).
  */
 import type { Config } from 'jest';
 
