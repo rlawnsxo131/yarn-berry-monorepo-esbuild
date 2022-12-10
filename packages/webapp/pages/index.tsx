5b36adca3d26250ee7baba99b9bd0d5c4a4efba6
juntae <rlawnsxo131@gmail.com>
2022-12-11 01:38:05 +0900

    add: 약간의 추가 수정

--- packages/webapp/pages/index.tsx
+++ packages/webapp/pages/index.tsx
@@ -5,7 +5,7 @@ import John from '@/components/John';
 export default function App() {
   return (
     <div>
-      <Button>click</Button>
+      <Button onClick={() => alert('hello')}>click</Button>
       <John />
     </div>
   );
